ef2cfd859750d1b42b73db19f1796fa4568b15a8
Eric Rustrum <48301203+MadKamel@users.noreply.github.com>
2022-02-04 15:35:18 -0800

    Added in more things and lost some sanity.
    WHAT THE FUCK IS WRONG WITH YOU MINETEST

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 dofile(minetest.get_modpath("mk_items").."/conveyor.lua")
+dofile(minetest.get_modpath("mk_items").."/miner.lua")
 
 
 
@@ -22,9 +23,19 @@ minetest.register_tool("mk_items:chisel", {
 	}
 })
 
+minetest.register_craft({
+	output = "mk_items:chisel",
+	recipe = {
+		{"mk_items:iron_frame"},
+		{"mk_items:copper_coil"},
+		{"mk_items:steel_rod"}
+	}
+})
+
 give_chisel = function(player)
 	local inv = player:get_inventory()
 	inv:add_item("main", ItemStack("mk_items:chisel"))
+	inv:add_item("main", ItemStack("default:furnace"))
 	if minetest.get_modpath("xenozapper") then
 		inv:add_item("main", ItemStack("xenozapper:zapper"))
 	end
@@ -47,12 +58,34 @@ minetest.register_node("mk_items:iron_deposit", {
 	end
 })
 
+minetest.register_node("mk_items:copper_deposit", {
+	description = "Copper Mineral Deposit",
+	tiles = {"mk_copper_node.png"},
+	groups = {deposit = 1, cracky = 2},
+	drop = "default:copper_lump",
+	node_dig_prediction = "mk_items:copper_deposit",
+	after_dig_node = function(pos, oldnode, oldmetadata, digger)
+		minetest.set_node(pos, {name="mk_items:copper_deposit"})
+	end
+})
+
+minetest.register_node("mk_items:coal_deposit", {
+	description = "Coal Mineral Deposit",
+	tiles = {"mk_coal_node.png"},
+	groups = {deposit = 1, cracky = 2},
+	drop = "default:coal_lump",
+	node_dig_prediction = "mk_items:coal_deposit",
+	after_dig_node = function(pos, oldnode, oldmetadata, digger)
+		minetest.set_node(pos, {name="mk_items:coal_deposit"})
+	end
+})
+
 minetest.register_decoration({
 	name = "mk_items:iron_deposit",
 	deco_type = "schematic",
 	place_on = {"default:dirt_with_grass", "default:dirt_with_snow", "default:dry_dirt_with_dry_grass"},
 	sidelen = 16,
-	fill_ratio = 0.001,
+	fill_ratio = 0.0005,
 	y_max = 64,
 	y_min = 3,
 	schematic = minetest.get_modpath("mk_items") .. "/schematics/iron_deposit.mts",
@@ -60,9 +93,39 @@ minetest.register_decoration({
 	rotation = "random",
 })
 
+minetest.register_decoration({
+	name = "mk_items:copper_deposit",
+	deco_type = "schematic",
+	place_on = {"default:dirt_with_grass", "default:dirt_with_snow", "default:dry_dirt_with_dry_grass"},
+	sidelen = 16,
+	fill_ratio = 0.0005,
+	y_max = 64,
+	y_min = 8,
+	schematic = minetest.get_modpath("mk_items") .. "/schematics/copper_deposit.mts",
+	flags = "place_center_x, place_center_z, force_placement",
+	rotation = "random",
+})
+
+minetest.register_decoration({
+	name = "mk_items:coal_deposit",
+	deco_type = "schematic",
+	place_on = {"default:dirt_with_grass", "default:dirt_with_snow", "default:dry_dirt_with_dry_grass"},
+	sidelen = 16,
+	fill_ratio = 0.00025,
+	y_max = 64,
+	y_min = 8,
+	schematic = minetest.get_modpath("mk_items") .. "/schematics/coal_deposit.mts",
+	flags = "place_center_x, place_center_z, force_placement",
+	rotation = "random",
+})
+
 
 
 -- Finally, the craftitems and other crafting recipes.
+minetest.override_item("default:furnace", {
+	groups = {oddly_breakable_by_hand = 3}
+})
+
 minetest.override_item("default:steel_ingot", {
 	description = "Iron Ingot"
 })
@@ -99,9 +162,9 @@ minetest.register_craftitem("mk_items:iron_frame", {
 minetest.register_craft({
 	output = "mk_items:iron_frame 2",
 	recipe = {
-		{"mk_items:iron_rod", "mk_items:iron_rod", "mk_items:iron_rod"},
+		{"", "mk_items:iron_rod", ""},
 		{"mk_items:iron_rod", "", "mk_items:iron_rod"},
-		{"mk_items:iron_rod", "mk_items:iron_rod", "mk_items:iron_rod"}
+		{"", "mk_items:iron_rod", ""}
 	}
 })
 
@@ -244,5 +307,20 @@ minetest.register_craft({
 		{"", "mk_items:framed_iron_plate", ""},
 		{"mk_items:framed_iron_plate", "mk_items:motor", "mk_items:framed_iron_plate"},
 		{"", "mk_items:framed_iron_plate", ""}
+	}
+})
+
+minetest.register_node("mk_items:foundation", {
+	description = "Metal Foundation",
+	tiles = {"mk_foundation_floor.png", "mk_foundation_wall.png"},
+	groups = {oddly_breakable_by_hand = 3}
+})
+
+minetest.register_craft({
+	output = "mk_items:foundation 12",
+	recipe = {
+		{"mk_items:iron_plate","mk_items:iron_plate", "mk_items:iron_plate"},
+		{"mk_items:iron_rod", "mk_items:iron_rod", "mk_items:iron_rod"},
+		{"mk_items:iron_plate","mk_items:iron_plate", "mk_items:iron_plate"}
 	}
 })
---
+++ miner.lua
@@ -0,0 +1,59 @@
+function under(pos)
+	pos_duplicate = pos
+	pos_duplicate.y = pos_duplicate.y - 1
+	return pos_duplicate
+end
+
+function above(pos)
+	pos_duplicate = pos
+	pos_duplicate.y = pos_duplicate.y + 1
+	return pos_duplicate
+end
+	
+local function update_meta(meta, enabled)
+	local state = enabled and "on" or "off"
+	meta:set_int("enabled", enabled and 1 or 0)
+	local fs = "formspec_version[4]size[10.5,11]list[current_player;main;0.4,5.9;8,4;0]list[context;dst;0.4,4.6;1,1;0]"
+	meta:set_string("formspec",fs)
+	local output = meta:get_inventory():get_stack("dst", 1)
+	if output:is_empty() then -- doesn't matter if paused or not
+		meta:set_string("infotext", "Portable Miner")
+		return false
+	end
+	return enabled
+end
+
+
+-- Register the Portable Miner
+minetest.register_node("mk_items:miner", {
+	description = "Portable Miner",
+	tiles = {
+		"mk_pminer_top.png",
+		"mk_pminer_bottom.png",
+		"mk_pminer_side.png"
+	},
+	groups = {oddly_breakable_by_hand = 3, falling_node = 1},
+	on_construct = function(pos)
+		local meta = minetest.get_meta(pos)
+		local inv = meta:get_inventory()
+		inv:set_size("dst", 1)
+		update_meta(meta, false)
+	end,
+	max_stack = 1
+})
+
+
+
+minetest.register_abm({
+	label = "Activate Miners",
+	nodenames = {"mk_items:miner"},
+	interval = 1,
+	chance = 1,
+	action = function(pos, node, aoc, aoc_wide)
+		if minetest.get_node(under(pos)).name == "mk_items:iron_deposit" then
+			minetest.get_meta(above(pos)):get_inventory():add_item('dst', 'default:iron_lump')
+		elseif minetest.get_node(above(under(pos))).name == "mk_items:coal_deposit" then
+			minetest.get_meta(above(pos)):get_inventory():add_item('dst', 'default:coal_lump')
+		end
+	end
+})
